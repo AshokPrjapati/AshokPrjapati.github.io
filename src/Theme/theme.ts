90bedcab67d2faf31d3f86fec0d21b0dbff79e44
Ashok Kumar <ap271449@gmail.com>
2025-09-25 20:55:02 +0530

    refactor: clean up theme.ts formatting and improve code readability

--- src/Theme/theme.ts
+++ src/Theme/theme.ts
@@ -1,32 +1,27 @@
 // theme.js
 
 // 1. import `extendTheme` function
-import { extendTheme } from '@chakra-ui/react'
-
-
-
-
+import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
-    // config: {
-    //     initialColorMode: 'dark',
-    //     useSystemColorMode: false,
-    // },
-    colors: {
-        text: {
-            1: "#fff",
-            2: "#a0a0a0",
-            3: "#e11d48",
-        },
-        bg: {
-            1: "#000000",
-            2: "#0d1117",
-            3: "#1e293b",
-            4: "#0284c7",
-        }
+  // config: {
+  //     initialColorMode: 'dark',
+  //     useSystemColorMode: false,
+  // },
+  colors: {
+    text: {
+      1: "#fff",
+      2: "#a0a0a0",
+      3: "#e11d48",
     },
-
-})
+    bg: {
+      1: "#000000",
+      2: "#0d1117",
+      3: "#1e293b",
+      4: "#0284c7",
+    },
+  },
+});
 
 // 3. extend the theme
 
