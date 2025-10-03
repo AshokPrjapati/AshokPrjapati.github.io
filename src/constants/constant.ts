24f1bedb8164e7af284a5b8cbebe1755a0061783
Ashok Kumar <ap271449@gmail.com>
2025-10-03 10:15:28 +0530

    refactor: update theme in GitHub stats links for consistent styling

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -45,8 +45,8 @@ export const RESUME_DRIVE_LINK =
   "https://drive.google.com/file/d/1P27IWs5s4X1JBKLqUAMapYS6an5D_wvH/view?usp=sharing";
 
 export const gitRepoLanguagesStates =
-  "https://github-readme-stats.vercel.app/api/top-langs/?username=AshokPrjapati&theme=radical&langs_count=8";
+  "https://github-readme-stats.vercel.app/api/top-langs/?username=AshokPrjapati&theme=transparent&show_icons=true&langs_count=8";
 export const gitContributeStates =
-  "https://github-readme-streak-stats.herokuapp.com?user=AshokPrjapati&theme=radical&border_radius=5.8&mode=daily";
+  "https://github-readme-streak-stats.herokuapp.com?user=AshokPrjapati&theme=transparent&border_radius=5.8&mode=daily&show_icons=true";
 
 export const PROFILE_EMAIL = "ashok.prjapati97@gmail.com";
