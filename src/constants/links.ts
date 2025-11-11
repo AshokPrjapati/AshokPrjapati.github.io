f20680d847706849ece7dfe8daf83ea6dd038c06
Ashok Kumar <ap271449@gmail.com>
2025-11-11 17:20:18 +0530

    fix: update resume drive link for accurate access

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -3,6 +3,6 @@ export const GITHUB_USERNAME = "AshokPrjapati";
 export const LINKEDIN_USERNAME = "ashok-kumar-1778b213b";
 
 export const RESUME_DRIVE_LINK =
-  "https://drive.google.com/file/d/1jrxKVeEHHldmfAXjxX0j0pEjrd7k-B0x/view?usp=drive_link";
+  "https://drive.google.com/file/d/1P27IWs5s4X1JBKLqUAMapYS6an5D_wvH/view?usp=sharing";
 export const GITHUB_LINK = `https://github.com/${GITHUB_USERNAME}`;
 export const LINKEDIN_LINK = `https://www.linkedin.com/in/${LINKEDIN_USERNAME}/`;
